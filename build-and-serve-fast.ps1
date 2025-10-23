16926b7d074b4fab55c26ded099fa1c436974d8f
Jerry Kim <42jerrykim@gmail.com>
2025-10-24 08:18:35 +0900

    section 포함

--- build-and-serve-fast.ps1
+++ build-and-serve-fast.ps1
@@ -1,4 +1,5 @@
 Write-Host "Hugo 개발 서버를 시작합니다..." -ForegroundColor Cyan
 
 # Hugo 개발 서버 시작 (live reload 활성화)
-hugo serve -D -F --port 12345 --disableKinds section,term,home --openBrowser --logLevel=debug --bind 0.0.0.0
+hugo serve -D -F --port 12345 --disableKinds term,home --openBrowser --logLevel=debug --bind 0.0.0.0
+#hugo serve -D -F --port 12345 --disableKinds section,term,home --openBrowser --logLevel=debug --bind 0.0.0.0
